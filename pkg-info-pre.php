b2fbc5920235939877dc0bd06ca40ac19d0f301f
huohongjian <1123576798@qq.com>
2017-08-28 22:04:39 +0800

    mm

---
+++ pkg-info-pre.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>FreeBSD Manual Translation</title>
+	<link rel="stylesheet" type="text/css" href="css/manual.css">
+</head>
+<body>
+	
+<div class="translation">
+<pre>
+
+
+<?php
+
+function __autoload($class){ include_once("class/$class.php");}
+
+$pgsql = PgSQL::getInstance();
+
+$sql = "SELECT * FROM m_content WHERE docid=2 ORDER BY id";
+
+$rs = $pgsql->fetchAll($sql);
+
+foreach($rs as $r){
+	print_r("<span data-id=\"{$r['id']}\">{$r['english']}</span>\n");
+
+}
+
+
+?>
+
+
+</pre>
+</div>
+
+
+
+</body>
+</html>
